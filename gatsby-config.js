54c05efaa62a0c9c1fb10bc6f26cf20a1e995042
Dries Bos <info@driesbos.com>
2019-12-19 22:07:12 +0100

    storyblok connect

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-storyblok",
+      options: {
+        accessToken: "TOKEN",
+        homeSlug: "home",
+        version: "draft",
+        // version: process.env.NODE_ENV === 'production' ? 'published' : 'draft'
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
